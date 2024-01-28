eb695438148d09d7fbbfc3ab264d1ac8c529b653
wellalencarweb <wellingtonlogia@gmail.com>
2024-01-28 17:12:02 -0300

    feat: cria estrutura para Categoria Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Src\BoundedContext\Client\Infrastructure\Eloquent\ClientModel;
+use Src\BoundedContext\Product\Domain\Enums\ProductCategoryEnum;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,5 +15,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         ClientModel::newFactoryTimes(1)->create(['cpf' => '95162320005']);
+
+        foreach(ProductCategoryEnum::values() as $key => $id) {
+            DB::table('product_categories')->updateOrInsert([
+                'id' => $id,
+                'description' => $key,
+            ]);
+        }
     }
 }
